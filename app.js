bd42c6410806778c974d1936b22b3947868a25ce
Mitchell Waldman <mitchswaldman@gmail.com>
2016-04-24 12:24:04 -0700

    use process.env

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(env.PORT || 8080);
+server.listen(process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
