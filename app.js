1cff691c9ea380d2335114c63ff51ce65d837b94
Mitchell Waldman <mitchswaldman@gmail.com>
2016-04-24 12:01:02 -0700

    Add in base code.

---
+++ app.js
@@ -0,0 +1,58 @@
+// // Including libraries
+
+// var app = require('http').createServer(handler),
+// 	io = require('socket.io').listen(app),
+// 	static = require('node-static'); // for serving files
+
+// // This will make all the files in the current folder
+// // accessible from the web
+// var fileServer = new static.Server('./');
+	
+// // This is the port for our web server.
+// // you will need to go to http://localhost:8080 to see it
+// app.listen(8080);
+
+// // If the URL of the socket server is opened in a browser
+// function handler (request, response) {
+
+// 	request.addListener('end', function () {
+//         fileServer.serve(request, response);
+//     });
+// }
+
+// // Delete this row if you want to see debug messages
+// //io.set('log level', 1);
+
+// // Listen for incoming connections from clients
+// io.sockets.on('connection', function (socket) {
+
+// 	// Start listening for mouse move events
+// 	socket.on('mousemove', function (data) {
+		
+// 		// This line sends the event (broadcasts it)
+// 		// to everyone except the originating client.
+// 		socket.broadcast.emit('moving', data);
+// 	});
+// });
+
+
+// PORT TO EXPRESS
+var express = require('express');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('mousemove', function (data) {
+    console.log(data);
+    socket.broadcast.emit('moving', data);
+  });
+});
